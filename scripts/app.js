ba5e636285331b4e0e1a9fb38e7d7a6fae99280b
Arne Schauf <git@as-webservices.de>
2015-05-22 13:43:02 +0200

    new build

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,1 @@
-!function(e){"use strict";var o=e.querySelector("#app");o.appName="Dumbledore",o.addEventListener("template-bound",function(){console.log("Our app is ready to rock!")})}(wrap(document));
+!function(e){"use strict";var t=e.querySelector("#app");t.addEventListener("template-bound",function(){console.log("Our app is ready to rock!")})}(wrap(document));
